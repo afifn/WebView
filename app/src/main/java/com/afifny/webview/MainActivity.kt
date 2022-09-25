fe43a578605a90e3d3df9cd35b8f43f7c66311f8
afifn <a@kuycoding.com>
2022-09-25 09:08:37 +0700

    Initial commit

---
+++ app/src/main/java/com/afifny/webview/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.afifny.webview
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.JsResult
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+
+        //configure
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                view?.loadUrl("javascript:alert('Web berhasil dimuat')")
+            }
+        }
+        //webChrome
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(
+                view: WebView?,
+                url: String?,
+                message: String?,
+                result: JsResult?
+            ): Boolean {
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+                result?.confirm()
+                return true
+            }
+        }
+        webView.loadUrl("https://www.afifny.my.id")
+    }
+}
